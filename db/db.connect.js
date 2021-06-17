7db7c00043e3ed126b2fcba8ea1eddb774077322
shubham-ghuge <shubhamghuge34@gmail.com>
2021-06-17 20:05:57 +0530

    finally fixed the deployment bug

--- db/db.connect.js
+++ db/db.connect.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 async function initialiseDbConnection() {
     try {
-        await mongoose.connect("mongodb+srv://shubham-ghuge:QW8r7KGSaZsDa1yb@farmers-grocery-cluster.qyup9.mongodb.net/real-farmer-quiz?retryWrites=true&w=majority", { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false });
+        await mongoose.connect(process.env.mongodb_uri, { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false });
         console.log("Database connected successfully")
     } catch (error) {
         console.log("Error in connecting to database", error);
